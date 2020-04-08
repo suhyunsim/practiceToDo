762f9fa3aeb90276c20fb17b24388438d10eb6cd
Suhyun <cnc1106@naver.com>
2020-04-08 09:32:13 +0900

    project setting

---
+++ src/main/java/com/codesquad/practiceToDo/PracticeToDoApplication.java
@@ -0,0 +1,13 @@
+package com.codesquad.practiceToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeToDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codesquad/practiceToDo/PracticeToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesquad.practiceToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
