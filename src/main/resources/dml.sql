0ed24f047fbc5f00f1f43d10fc237e16b0ca91d6
suhyunsim <cnc1106@naver.com>
2020-04-10 12:41:04 +0900

    Refactor: sample data 영문 변

--- src/main/resources/dml.sql
+++ src/main/resources/dml.sql
@@ -1,14 +1,14 @@
 INSERT INTO
     Category(Id, Name, Status)
 VALUES
-    (1, '해야 할 일', TRUE),
-    (2, '하고 있는 일', TRUE),
-    (3, '완료된 일', TRUE);
+    (1, 'todo', TRUE),
+    (2, 'progress', TRUE),
+    (3, 'done', TRUE);
 
 INSERT INTO
-    Card(Id, Contents, Created_Time, Modified_Time, Category)
+    Card(Id, Contents, Created_Time, Modified_Time, Status, Category)
 VALUES
-    (1, 'Spring boot와 MySQL 연결', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), TRUE, 3),
-    (2, 'Spring data jdbc 학습', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), TRUE, 2),
-    (3, 'ddd aggregate root 선택 기준', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), TRUE, 1),
-    (4, 'aggregate root를 통해 CRUD 로직 구현', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), TRUE, 1);
+    (1, 'Spring boot', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), TRUE, 3),
+    (2, 'Spring data', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), TRUE, 2),
+    (3, 'ddd aggregate', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), TRUE, 1),
+    (4, 'aggregate root', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), TRUE, 1);
